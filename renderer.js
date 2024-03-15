65959389ed9819e9e8d993de6e337a7ead83f357
he55 <40764272+he55@users.noreply.github.com>
2024-03-15 20:01:59 +0800

    fix mouse enter event

--- renderer.js
+++ renderer.js
@@ -6,41 +6,44 @@
  * to expose Node.js functionality from the main process.
  */
 
-async function loadData(){
-    let data=await ipc.getWallpapers()
-    let tag_list=data.tag_list
-    let wallpaper_list=data.list
+async function loadData() {
+    let data = await ipc.getWallpapers()
+    let tag_list = data.tag_list
+    let wallpaper_list = data.list
 
-    let left_ul=document.querySelector('.left')
-    tag_list.forEach((item,idx)=>{
-        let li=document.createElement('li')
-        li.textContent=item.tag_name
-        if(idx===2){
-            li.className='active'
+    let left_ul = document.querySelector('.left')
+    tag_list.forEach((item, idx) => {
+        let li = document.createElement('li')
+        li.textContent = item.tag_name
+        if (idx === 2) {
+            li.className = 'active'
         }
         left_ul.appendChild(li)
     })
 
-    let main_li=document.querySelector('.main')
-    wallpaper_list.forEach((item,idx)=>{
-        let li=document.createElement('li')
-        li.onmouseenter=function(){
-            if(item.type===1){
-                this.innerHTML=`<video autoplay loop><source src=${item.res}></video>`
+    let main_li = document.querySelector('.main')
+    wallpaper_list.forEach((item, idx) => {
+        let li = document.createElement('li')
+        if (item.type === 1) {
+            function mouseenter() {
+                li.onmouseenter = null
+                this.innerHTML = `<video autoplay loop><source src=${item.res}></video>`
+                console.log('mouseenter', item.res)
+
+                li.onmouseleave = function () {
+                    this.innerHTML = `<img src="${item.img}">`
+                    console.log('mouseleave')
+                    li.onmouseenter = mouseenter
+                }
             }
-            console.log('mouseenter',item.res)
+            li.onmouseenter = mouseenter
         }
-        li.onmouseleave=function(){
-            if(item.type===1){
-                this.innerHTML=`<img src="${item.img}">`
-            }
-            console.log('mouseleave')
+
+        if (idx === 5) {
+            li.className = 'active'
         }
-        if(idx===5){
-            li.className='active'
-        }
-        let img =new Image()
-        img.src=item.img
+        let img = new Image()
+        img.src = item.img
         li.appendChild(img)
         main_li.appendChild(li)
     })
