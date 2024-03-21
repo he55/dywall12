75f605eb1e4c0b5cbc16bfa8198a70b5bd450e11
he55 <40764272+he55@users.noreply.github.com>
2024-03-21 23:58:39 +0800

    Update main.js

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ function createWindow() {
    mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    autoHideMenuBar:true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
